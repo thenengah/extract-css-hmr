082e92c27a7bc3e754fd80e6c8e9db42a7d6fcec
thenengah <thenengah@gmail.com>
2018-01-18 11:31:41 -0800

    fixed ordering of css in plugin

--- src/plugin.js
+++ src/plugin.js
@@ -1,16 +1,21 @@
 import fs from 'fs'
 import getHashDigest from 'loader-utils/lib/getHashDigest'
 
-module.exports = function ({filename}) {
+module.exports = function ({filename}) { // rename filename to format filename
   this.apply = compiler => {
     compiler.plugin('emit', (compilation, next) => {
       const hmrCssEntries = compilation.hmrCssEntries || {}
       Object.keys(hmrCssEntries).forEach(entry => {
-        const content = Object.keys(hmrCssEntries[entry]).sort((a, b) =>
-          a.position - b.position
-        ).map(resourcePath =>
-          hmrCssEntries[entry][resourcePath].style
-        ).join('')
+        const content = Object.keys(hmrCssEntries[entry]).map(file => {
+          const {position, style} = hmrCssEntries[entry][file]
+          return {file, position, style}
+        }).sort((a, b) =>
+          b.position - a.position
+        ).map(obj => {
+          console.log(obj.position, obj.file)
+          return obj.style
+        }).join('')
+
         const hash = getHashDigest(content, 'md5', 'base64', 10)
         filename = filename.replace(/\[name\]/, entry).replace(/\[hash\]/, hash)
         compilation.assets[filename] = {
